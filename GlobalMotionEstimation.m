93b86d8af93cf1514bd61e28fea274888df4785e
Andrew Spittlemeister <andyspitts@gmail.com>
2017-10-31 15:48:54 -0500

    added comments to describe gradient descent method

--- GlobalMotionEstimation.m
+++ GlobalMotionEstimation.m
@@ -13,20 +13,29 @@ function GlobalMotionEstimation(Mask, Iref, Ireg)
     end
     
     if flag
-        InitialError = Intercorrelation(IregMasked, IrefMasked,Mask);
+        PrevError = Intercorrelation(IregMasked, IrefMasked,Mask);
     else
-        InitialError = MutualInformation(IregMasked, IrefMasked, 100);
+        PrevError = MutualInformation(IregMasked, IrefMasked, 100);
     end
     
     ErrorChange = inf;
     M = [1, 0; 0, 1];
     D = [0 ; 0];
-    % Start Gradient Descent.
-    while ErrorChange < 1
-        if flag
-            % postive M11 change:
+    
+    if flag % Start Gradient Descent for Intercorrelation error:
+        while ErrorChange < 1
+            % for each of the 6 parameters vary their values positively and negatively.
+            % after each change calculate the respective error function value.
+            % this will produce 6 x 2 matrix of errors (col 1 for postive changes, col 2 for negative).
+                  % rows are denoted from top to bottom: M11, M12, M21, M22, D1, D2
+            % for each row determine which error is less, and either the positve or negative action for 
+               % that parameter will be implemented.
+            % implement these changes onto the IregMasked matrix and its Mask.
+            % Recalculate the error for the newly changed image and store this in CurError.
+            % Assign abs(CurError - PrevError) to ErrorChange for the while loop conditional.
             M(1,1) = M(1,1) + (1/xlength);
-            
+                    
+    else % Start gradient descent for mutual information error:           
             
             
     
