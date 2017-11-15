54c457548b5ce2e50f519c571e48dd5bcd6b5f77
Andrew Spittlemeister <andyspitts@gmail.com>
2017-11-15 12:40:00 -0600

    debuggin

--- ImageShift.m
+++ ImageShift.m
@@ -1,33 +1,45 @@
-image = dicomread('ACCMR_VOL_020.MR.AKCAKAYA_VE11C_SETUP.0013.0001.2017.08.18.16.08.30.466382.36253287.IMA');
+image = dicomread('img3.dcm');
+figure;
+imshow(image,[]);
 TestMask = ones(176,176);
 WastMask = ones(176,176);
-M = [1,0; 0,1];
+M = [0.707,-0.707; 0.707,0.707];
 D = [0;0];
 ImageShiftFunc(image, TestMask, M, D);
 function [NewIreg, NewMask] = ImageShiftFunc(Ireg, Mask, Mnew, D)
-    JointMatrix = cat(1,cat(2,Mnew,D),[0,0,1]);
+    JointMatrix = cat(1,cat(2,Mnew,D),[0,0,1])
     xlength = length(Ireg(1,:));
     ylength = length(Ireg(:,1));
     
+    PreTranslate = [1, 0, -xlength/2; 0, 1, ylength/2; 0, 0, 1]
+    PostTranslate = [1, 0, xlength/2; 0, 1, -ylength/2; 0, 0, 1]
+    
+    %TransMatrix = PostTranslate * JointMatrix * PreTranslate;
+    TransMatrix = PreTranslate;
     NewIreg = zeros(xlength, ylength);
     
     %Use Reverse Mapping to get New Image
     for x = 1:xlength
         for y = 1:ylength
+            if x <= 50 && y <= 50
+                Ireg(y,x) = 0;
+            end
             loc = [x;y;1];
-            NewLoc = JointMatrix * loc;
-            NewLoc(1,1) = int16(NewLoc(1,1));
-            NewLoc(2,1) = int16(NewLoc(2,1));
+            %NewLoc = TransMatrix * loc;
+            NewLoc = loc;
+            NewLoc(1) = round(NewLoc(1));
+            NewLoc(2) = round(NewLoc(2));
             %Boundary check for our NewLoc
-            if NewLoc(1,1) >= 1 && NewLoc(2,1) >= 1
-                if NewLoc(1,1) <= xlength && NewLoc(2,1) <= ylength
+            if NewLoc(1) >= 1 && NewLoc(2) >= 1
+                if NewLoc(1) <= xlength && NewLoc(2) <= ylength
                     %Currently using simple point sampling. Could be improved using different sampling method
-                    NewIreg(x,y) = Ireg(NewLoc(1), NewLoc(2));
+                    NewIreg(y,x) = Ireg(NewLoc(1), NewLoc(2));
                 end
             end
         end
     end
-    NewIreg = Mask .* NewIreg
+    NewIreg = Mask .* NewIreg;
+    figure;
+    imshow(NewIreg, []);
     
 end
-
