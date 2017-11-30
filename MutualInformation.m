64e34c3a0570e069cd6a33a61cad0a53dc7002e4
voasx005 <voasx005@umn.edu>
2017-11-29 22:53:40 -0600

    Image shift code and updated mutual information code
    Still not working correctly. Not sure whats wrong and mehmet hasn't responded much about it.

--- MutualInformation.m
+++ MutualInformation.m
@@ -1,21 +1,19 @@
 function [E] = MutualInformation(Ireg, Iref, Mask, N)
-    
-    % Mask Images:
-    [Ireg, Iref] = MaskImages(Ireg,Iref,Mask);
-    
-    Ireg = Ireg(:);        % cast to array
-    Ireg = Ireg(Ireg ~= 0);% get rid of all zeros
-    % do same for Iref
-    Iref = Iref(:);
-    Iref = Iref(Iref ~= 0);
-
-    Pref = hist(Iref(:), N);
-    Preg = hist(Ireg(:), N);
-    Joint = hist3([Iref(:),Ireg(:)], [N,N]);
+    Ireg = double(Ireg(Mask == 1));
+    Iref = double(Iref(Mask == 1));
+    Pref = hist(Iref, N);
+    Preg = hist(Ireg, N);
+    data = [Iref,Ireg];
+    Joint = hist3(data, [N,N]);
     E = 0;
     for n1=1:N
         for n2=1:N
-            E = E + Joint(n1,n2) * log((Joint(n1,n2)/(Pref(n1)*Preg(n2))));
+            if ~(Joint(n1, n2) == 0 || Pref(n1) == 0 || Preg(n2) == 0)
+                E = E + Joint(n1,n2) * abs(log((Joint(n1,n2)/(Pref(n1)*Preg(n2)))));
+            end
+           
         end
     end
+    E
+end
     
