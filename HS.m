5730291663a4c9187cb26b7fbad88486062f1b8a
Andrew Spittlemeister <andyspitts@gmail.com>
2017-12-17 23:08:31 -0600

    Update HS.m

--- HS.m
+++ HS.m
@@ -17,7 +17,7 @@ u = zeros(size(im1));
 v = zeros(size(im1));
 
 % smooth the image with Gaussian Filter
-segma = 1;
+segma = 1.5;
 kSize=2*(segma*3);
 x=-(kSize/2):(1+1/kSize):(kSize/2);
 G=(1/(sqrt(2*pi)*segma)) * exp (-(x.^2)/(2*segma^2));
